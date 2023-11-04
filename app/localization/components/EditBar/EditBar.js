f2e183db738089ad9218a2254b986ecfb3e51295
4N1Z <20am014@sctce.ac.in>
2023-11-04 20:25:11 +0530

    edit card added

--- app/localization/components/EditBar/EditBar.js
+++ app/localization/components/EditBar/EditBar.js
@@ -1,52 +1,34 @@
-'use client'
+"use client";
 
-import React , {useEffect} from "react";
+import React from "react";
+// import React, { useEffect } from "react";
+import dynamic from "next/dynamic";
 import "./EditBar.css";
 import { dots } from "@/app/svgs/dots";
 import Image from "next/image";
 
+const Model = dynamic(() => import("./Model"), { ssr: false });
+
 function EditBar() {
 
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.type = "module";
-    script.src =
-      "https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js";
-    document.head.appendChild(script);
-
-    return () => {
-      document.head.removeChild(script); // Clean up the script on component unmount
-    };
-  }, []); 
-  
   return (
     <div className="editContainer">
       {/* <Image src={dots} alt="dots svg" /> */}
 
       {/* <div className="editSubContainer"> */}
-        <div className="editViewGroup">
-          <div className=" editBar ARroom">
-            <p>AR Room</p>
-          </div>
-
-          <model-viewer
-          alt="Neil Armstrong's Spacesuit from the Smithsonian Digitization Programs Office and National Air and Space Museum"
-          src="shared-assets/models/NeilArmstrong.glb"
-          ar
-          environment-image="shared-assets/environments/moon_1k.hdr"
-          poster="shared-assets/models/NeilArmstrong.webp"
-          shadow-intensity="1"
-          camera-controls
-          touch-action="pan-y"
-        ></model-viewer>
-
-
-          <div className="editBar editingTools">
-            <p>Editing Tools</p>
-          </div>
+      <div className="editViewGroup">
+        <div className=" editBar ARroom">
+          <p>AR Room</p>
         </div>
-      {/* </div> */}
+        <div className="modelContainer">
+          <Model/>;
+        </div>        
 
+        <div className="editBar editingTools">
+          <p>Editing Tools</p>
+        </div>
+      </div>
+      {/* </div> */}
     </div>
   );
 }
