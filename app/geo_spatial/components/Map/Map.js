f30be16d91cdcbf322aeefcb36d1c7cc4fe14d4f
Anirudh Dayanand <anirudhdaya@gmail.com>
2023-11-04 07:45:00 +0530

    Added maps

--- app/geo_spatial/components/Map/Map.js
+++ app/geo_spatial/components/Map/Map.js
@@ -1,13 +1,63 @@
 "use client";
-import React from 'react'
-import "./Map.css"
+import React, { useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import './Map.css';
+const Map = () => {
+  const mapContainerStyle = {
+    width: '800px',
+    height: '800px',
+  };
 
-function Map() {
+  const center = {
+    lat: 27.672932021393862,
+    lng: 85.31184012689732,
+  };
+
+  const [markers, setMarkers] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  const handleMapClick = (event) => {
+    // Add a new marker at the clicked position
+    const newMarker = {
+      lat: event.latLng.lat(),
+      lng: event.latLng.lng(),
+    };
+    setMarkers([...markers, newMarker]);
+  };
+
+  const handleMarkerClick = (marker) => {
+    // Display the GPS data when a marker is clicked
+    setSelectedMarker(marker);
+  };
+
   return (
-    <div className='map'>
-      
+    <div className="map">
+      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY}>
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          zoom={14}
+          center={center}
+          mapTypeId="hybrid"
+          onClick={handleMapClick} // Attach the click event handler
+        >
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              position={marker}
+              onClick={() => handleMarkerClick(marker)}
+            />
+          ))}
+        </GoogleMap>
+      </LoadScript>
+      {selectedMarker && (
+        <div className="marker-info">
+          <p>Latitude: {selectedMarker.lat}</p>
+          <p>Longitude: {selectedMarker.lng}</p>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Map
+export default Map;
+
