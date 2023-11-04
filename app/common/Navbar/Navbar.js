5790bcc4d2916a6993ab2e1bab7657a63f23a2f8
4N1Z <20am014@sctce.ac.in>
2023-11-04 16:17:33 +0530

    link added

--- app/common/Navbar/Navbar.js
+++ app/common/Navbar/Navbar.js
@@ -2,20 +2,21 @@ import Link from "next/link";
 import "./Navbar.css";
 import { logo } from "@/app/svgs";
 function Navbar() {
-  
-    return (
-      <div className="nav-container">
-        <nav>
-        <div className="nav-logo">
-          {logo}
-          <span className="nav-logo-text">AR Space</span>
-        </div>
+  return (
+    <div className="nav-container">
+      <nav>
+        {/* <Link href={"/"}>   */}
+          <Link href={"/"} className="nav-logo">
+            {logo}
+            <span className="nav-logo-text">AR Space</span>
+          </Link>
+        {/* </Link> */}
         <div className="nav-menu">
-          <Link href={'#'}>Login</Link>
+          <Link href={"#"}>Login</Link>
         </div>
       </nav>
-      </div>
-      )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
