9d3cc038e4beb06abe739a68956b1d048dfe9680
4N1Z <20am014@sctce.ac.in>
2023-11-04 08:50:58 +0530

    framer adding

--- app/components/Hero/Hero.js
+++ app/components/Hero/Hero.js
@@ -1,20 +1,26 @@
 import "./Hero.css";
 import { logo } from "@/app/svgs";
 import { hero } from "@/app/data";
+import { motion } from "framer-motion"
 
 function Hero() {
+
+
   return (
+    
     <div className="hero-container">
+      
       <div className="hero-logo">
         {logo}
-        <span className="hero-logo-text">AR Space</span>
+        <motion.span whileHover={{ scale: 1.1}} 
+        className="hero-logo-text">AR Space</motion.span>
       </div>
       <div className="hero-content">
         <p className="hero-description">
         {hero.catch_line}
         </p>
       </div>
-      
+
         <div className="button-group">
             <button className="button hero-button">Localization</button>
             <span className="divider"></span>
